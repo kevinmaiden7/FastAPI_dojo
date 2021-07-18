a3393331b84006d68f6b614c29e7235a22571261
Kevin Martinez <kevin.m.gallego@gmail.com>
2021-07-18 11:12:20 -0500

    Add endpoint with query params

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI() # Create a FastAPI instance
 @app.get("/") # Create a path operation (HTTP methods)
 def root(): # Define the path operation function
     return {"message": regards("root")} # Return a JSON
+
+@app.get("/name")
+def get_name(input_name):
+    return {"message": "Hello " + input_name + ", Welcome!"}
